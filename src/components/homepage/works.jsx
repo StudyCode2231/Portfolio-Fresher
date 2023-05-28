0087fa1bdfb738728224c6ca1c323ec7874d1813
StudyCode2231 <1903044@kiit.ac.in>
2023-05-28 11:19:34 +0530

    :zap: portfolio  commit

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -32,11 +32,7 @@ const Works = () => {
 							<div className="work-duration">Jan ,2022 -  May ,2022</div>
 						</div>
 						<div className="work">
-							<img
-								src="./Hrc.png"
-								alt="Hrc"
-								className="work-image"
-							/>
+							
 							<div className="work-title"></div>
 							<div className="work-subtitle">
 								
